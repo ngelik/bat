bdfb6532dc4ca806b3c50e2f6536d680524c7c23
aeliseev <k800elik@gmail.com>
2014-05-28 11:30:14 +0300

    RabbitMQ - improved

--- RabbitMQ/create_schedule_task.bat
+++ RabbitMQ/create_schedule_task.bat
@@ -1,21 +1,52 @@
 @echo off
 
-@rem Имя локального пользователя, под которым будетм работать
-set user_name=user_name
-@rem Пароль для локального пользователя
-set user_passw=
-@rem Имя запланированного задание, под которым будет работать приложение
+:: ***********************************************************************************************
+:: Задаем значения переменных
+:: ***********************************************************************************************
+:: Текущий путь
+set current_path=d:\RabbitMQ\install
+
+:: Имя локального пользователя, под которым будем работать
+set user_name=rabbitmq
+:: Пароль для локального пользователя
+set user_passw=Pa$$w0rd
+
+:: Имя запланированного задание, под которым будет работать приложение
 set task_name=rabbitmq.logrotate
-@rem Путь к приложению
+:: Путь к приложению
 set my_app_path="d:\RabbitMQ\install\logrotate_rabbitmq.bat "
-@rem Интервал работы приложения во временном задании
+:: Интервал работы приложения во временном задании
 set schtasks_time=DAILY
 set schtasks_time_intervale=1
-@rem Начальное время старта приложения во временном задании
-set schtasks_start=00:00:01
+:: Начальное время старта приложения во временном задании
+set schtasks_start=23:59:00
 
-@REM Создание запланированного задания
-@rem  Valid schedule types: MINUTE, HOURLY, DAILY, WEEKLY, MONTHLY, ONCE, ONSTART, ONLOGON, ONIDLE.
-@rem ЕЖЕМИНУТНО, ЕЖЕЧАСНО, ЕЖЕДНЕВНО, ЕЖЕНЕДЕЛЬНО, ЕЖЕМЕСЯЧНО ПРИ ЗАПУСКЕ ВХОДЕ В СИСТЕМУ ПРИ ПРОСТОЕ
+:: ***********************************************************************************************
+:: В зависимости от языка ОС определяем имена системных параметров
+:: ***********************************************************************************************
+:: S-1-5-32-545 - локальные пользователи
+:: S-1-5-32-544 - администраторы
+Set GroupSID=S-1-5-32-545
+Set GroupName=
+For /F "UseBackQ Tokens=1* Delims==" %%I In (`WMIC Group Where "SID = '%GroupSID%'" Get Name /Value ^| Find "="`) Do Set GroupName=%%J
+Set GroupName=%GroupName:~0,-1%
+:: ***********************************************************************************************
+
+:: Создание пользователя
+net user %user_name% %user_passw% /add  /comment:"Simple user with Batch logon privilege for rotation RabbitMQ logs. Don't delete!" /expires:never /fullname:%user_name% /passwordchg:no
+:: Устанавливаем, чтобы пароль не истекал никогда
+:: Либо так - wmic path Win32_UserAccount where Name='%user_name%' set PasswordExpires=false
+wmic USERACCOUNT where Name='%user_name%' set PasswordExpires=false
+:: Добавление локального пользователя в заданную локальную группу
+net localgroup %GroupName% %user_name% /ADD
+
+:: ***********************************************************************************************
+:: Даем свежесозданному пользователю права Batch logon privilege
+:: ***********************************************************************************************
+%current_path%\tools\ntrights.exe -u %user_name% +r SeBatchLogonRight
+
+:: Создание запланированного задания
+::  Valid schedule types: MINUTE, HOURLY, DAILY, WEEKLY, MONTHLY, ONCE, ONSTART, ONLOGON, ONIDLE.
+:: ЕЖЕМИНУТНО, ЕЖЕЧАСНО, ЕЖЕДНЕВНО, ЕЖЕНЕДЕЛЬНО, ЕЖЕМЕСЯЧНО ПРИ ЗАПУСКЕ ВХОДЕ В СИСТЕМУ ПРИ ПРОСТОЕ
 schtasks /create /tn "%task_name%" /tr %my_app_path% /sc %schtasks_time% /MO %schtasks_time_intervale% /st %schtasks_start% /ru %user_name% /rp %user_passw%
 
--- RabbitMQ/logrotate_rabbitmq.bat
+++ RabbitMQ/logrotate_rabbitmq.bat
@@ -1,10 +1,15 @@
 @echo off
 
 set current_path=d:\RabbitMQ\install
+set log_path=d:\RabbitMQ\log
 set rabbitmqctl="c:\Program Files (x86)\RabbitMQ Server\rabbitmq_server-3.3.1\sbin\rabbitmqctl"
 
 for /f "delims=" %%a in ('%current_path%\tools\date +%%Y%%m%%d') do @set datetime=%%a
 
-call %rabbitmqctl% rotate_logs .%datetime%.log
+call %rabbitmqctl% rotate_logs .%datetime%
+
+:: for debug
+:: move /Y %log_path%\*.%datetime% %log_path%\old 1>>%current_path%\test.txt 2>>&1
+move /Y %log_path%\*.%datetime% %log_path%\old
 
 
